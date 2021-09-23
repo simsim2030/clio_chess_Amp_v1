cbe6426acadc77af31a3b54e5cf76e1893db6de9
Simeon Cheuk <etsimeon2030@gmail.com>
2021-09-23 17:34:05 +1000

    adding category on homepage

---
+++ lib/home_page_model/homepage_category.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+class HomeCategory {
+  final String id;
+  final String title;
+  final Color color;
+
+  const HomeCategory({@required this.id, @required this.title, @required this.color});
+}
---
+++ lib/home_page_model/homepage_data.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import 'homepage_category.dart';
+
+const HOMEPAGE_CATEGORIES = const [
+  HomeCategory(
+    id: 'c1',
+    title: 'Chessboard',
+    color: Colors.amber,
+  ),
+  HomeCategory(
+    id: 'c2',
+    title: 'Live Chess',
+    color: Colors.blueAccent,
+  ),
+];
