ee0244508c08737e3d14223df85b9b264f7476e7
Simeon Cheuk <etsimeon2030@gmail.com>
2022-05-03 17:21:56 +1000

    Finished the frontend appearents logic

--- lib/Screens/Login/components/login_body.dart
+++ lib/Screens/Login/components/login_body.dart
@@ -4,7 +4,6 @@ import 'package:clio_chess_app/Screens/Signup/signup_page.dart';
 import 'package:clio_chess_app/components/rounded_button.dart';
 import 'package:clio_chess_app/components/rounded_input_field.dart';
 import 'package:clio_chess_app/components/rounded_password_field.dart';
-import 'package:clio_chess_app/components/text_field_container.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -17,40 +16,41 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Background(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            "LOGIN",
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          SizedBox(height: size.height * 0.03),
-          SvgPicture.asset(
-            "assets/icons/login.svg",
-            height: size.height * 0.35,
-          ),
-          SizedBox(height: size.height * 0.03),
-          RoundedInputField(
-            hintText: "Email",
-            onChanged: (value) {},
-          ),
-          RoundedPasswordField(
-            hintText: "Password",
-            onChanged: (value) {},
-          ),
-          RoundedButton(
-            text: "LOGIN",
-            press: () {},
-          ),
-          SizedBox(height: size.height * 0.03),
-          AlreadyHaveAnAccountCheck(
-            press: () {
-              Navigator.of(context).pushNamed(SignupPage.routeName);
-            },
-          ),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "LOGIN",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: size.height * 0.03),
+            SvgPicture.asset(
+              "assets/icons/login.svg",
+              height: size.height * 0.35,
+            ),
+            SizedBox(height: size.height * 0.03),
+            RoundedInputField(
+              hintText: "Email",
+              onChanged: (value) {},
+            ),
+            RoundedPasswordField(
+              hintText: "Password",
+              onChanged: (value) {},
+            ),
+            RoundedButton(
+              text: "LOGIN",
+              press: () {},
+            ),
+            SizedBox(height: size.height * 0.03),
+            AlreadyHaveAnAccountCheck(
+              press: () {
+                Navigator.of(context).pushNamed(SignupPage.routeName);
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
 }
-
--- lib/Screens/Signup/components/signup_body.dart
+++ lib/Screens/Signup/components/signup_body.dart
@@ -1,6 +1,8 @@
 import 'package:clio_chess_app/Screens/Login/components/already_have_an_account_check.dart';
 import 'package:clio_chess_app/Screens/Login/login_page.dart';
+import 'package:clio_chess_app/Screens/Signup/components/or_divier.dart';
 import 'package:clio_chess_app/Screens/Signup/components/signup_background.dart';
+import 'package:clio_chess_app/Screens/Signup/components/social_icon.dart';
 import 'package:clio_chess_app/components/rounded_button.dart';
 import 'package:clio_chess_app/components/rounded_input_field.dart';
 import 'package:clio_chess_app/components/rounded_password_field.dart';
@@ -12,39 +14,66 @@ class SignupBody extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Background(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            "SIGNUP",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "SIGNUP",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-          SvgPicture.asset(
-            "assets/icons/signup.svg",
-            height: size.height * 0.35,
-          ),
-          RoundedInputField(
-            hintText: "Your Email",
-            onChanged: (value) {},
-          ),
-          RoundedPasswordField(
-            hintText: "Password",
-            onChanged: (value) {},
-          ),
-          RoundedButton(
-            text: "SIGNUP",
-            press: () {},
-          ),
-          AlreadyHaveAnAccountCheck(
-            login: false,
-            press: () {
-              Navigator.of(context).pushNamed(LoginPage.routeName);
-            },
-          ),
-        ],
+            SizedBox(
+              height: size.height * 0.03,
+            ),
+            SvgPicture.asset(
+              "assets/icons/signup.svg",
+              height: size.height * 0.35,
+            ),
+            RoundedInputField(
+              hintText: "Your Email",
+              onChanged: (value) {},
+            ),
+            RoundedPasswordField(
+              hintText: "Password",
+              onChanged: (value) {},
+            ),
+            RoundedButton(
+              text: "SIGNUP",
+              press: () {},
+            ),
+            SizedBox(
+              height: size.height * 0.03,
+            ),
+            AlreadyHaveAnAccountCheck(
+              login: false,
+              press: () {
+                Navigator.of(context).pushNamed(LoginPage.routeName);
+              },
+            ),
+            OrDivider(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SocialIcon(
+                  iconSrc: "assets/icons/facebook.svg",
+                  press: () {},
+                ),
+                SocialIcon(
+                  iconSrc: "assets/icons/twitter.svg",
+                  press: () {},
+                ),
+                SocialIcon(
+                  iconSrc: "assets/icons/google-plus.svg",
+                  press: () {},
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
 }
+
